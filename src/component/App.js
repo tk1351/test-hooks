45ec7e4fe645a8877143d7902b80a3a0f5db654f
TK1351 <takakikitsui0111@gmail.com>
2020-07-17 15:47:13 +0900

    reducer2 学習

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducer/index'
 
-const App = props => {
-  
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+ 
+  const addEvent = e => {
+    e.preventDefault()
+
+    dispatch( {
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('')
+    setBody('')
+  }
+
 
   return (
     <div className="container-fluid">
@@ -10,14 +27,14 @@ const App = props => {
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle"/>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody"/>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
@@ -34,6 +51,8 @@ const App = props => {
         <tbody></tbody>
       </table>
     </div>
+
+    
   );
 }
 
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -16,11 +16,11 @@ const events = (state = [], action) => {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body }
       const length = state.length
-      const id = length === 0 ? 1 : state[length -1].id + 1
+      const id = length === 0 ? 1 : state[length - 1].id + 1
       return [...state, { id, ...event }]
     case 'DELETE_EVENT':
       return state
-    case 'DELETE_ALL_EVENT':
+    case 'DELETE_ALL_EVENTS':
       return []
     default:
       return state
