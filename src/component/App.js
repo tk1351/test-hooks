53909910340b1670f15dacc5b1d269d6a5226d4e
TK1351 <takakikitsui0111@gmail.com>
2020-07-17 16:18:05 +0900

    dispatch学習

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducer/index'
+import Event from './event'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -48,7 +49,9 @@ const App = () => {
             <th></th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+        </tbody>
       </table>
     </div>
 
