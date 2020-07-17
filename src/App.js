f0f455d3f9c6fa13ac664147658bd8eb9f7c6bd6
TK1351 <takakikitsui0111@gmail.com>
2020-07-17 10:12:52 +0900

    useEffect callback学習

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(() => {
+    console.log('This is Like componentDidMount or componentDidUpdate')
+  })
+
+  useEffect(() => {
+    console.log('This is Like componentDidMount')
+  }, [])
+
+  useEffect(() => {
+    console.log('This callback is for name only')
+  }, [name])
 
   return (
     <>
