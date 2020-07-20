396a89ebee11fd6c1891943c305ed8d88816e4e6
TK1351 <takakikitsui0111@gmail.com>
2020-07-20 10:13:40 +0900

    propDrilling解決

--- src/component/App.js
+++ src/component/App.js
@@ -5,16 +5,14 @@ import EventForm from './EventForm'
 import Events from './Events'
 import AppContext from '../contexts/AppContext';
 
-console.log({ AppContext })
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
-    <AppContext.Provider value={'Hello, I am a Provider.'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
