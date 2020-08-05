c7559e5321d703b92f143c5a720c86c8ea409963
TK1351 <takakikitsui0111@gmail.com>
2020-08-06 08:12:25 +0900

    hot-fix

--- src/component/EventForm.js
+++ src/component/EventForm.js
@@ -38,7 +38,7 @@ const EventForm = () => {
     if (result) {
       dispatch({ type: DELETE_ALL_EVENTS })
       dispatch({ 
-        type: DELETE_ALL_OPERATION_LOGS,
+        type: ADD_OPERATION_LOG,
         description: '全てのイベントを削除しました。',
         operatedAt: timeCurrentIso8601()
       })
