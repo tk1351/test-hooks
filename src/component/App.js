a9f0c9a302e6a11e69b3d77d42c7f5f4da3b19a0
TK1351 <takakikitsui0111@gmail.com>
2020-08-06 09:02:20 +0900

    操作ログ一覧の表示

--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducer'
 import EventForm from './EventForm'
 import Events from './Events'
+import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext';
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   )
